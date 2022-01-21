8f481d1ebf9612c76ae79deaa9cd4b59907bf8c5
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2022-01-20 23:14:34 -0800

    Add files via upload

---
+++ isPalinedrome.java
@@ -0,0 +1,45 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class isPalinedrome {
+    public static void main(String [] args) {
+
+        Boolean check = true;
+        // Keep the program running if the user wants to
+        while (check) {
+            // Prompt user to input a string
+            System.out.println("Enter a string to check if it's a palindrome or not: ");
+            Scanner scan = new Scanner(System.in);
+            String userInput = scan.next();
+            // Check to see if it's a palindrome or not
+            if (isPalindrome(userInput)) {
+                System.out.println("It is a palindrome");
+            } else {
+                System.out.println("It is not a palindrome");
+            }
+            // Ask for user input whether they want to check another string
+            System.out.println("Do you want to try another string?");
+            String input = scan.next().toLowerCase();
+            // Only exit when the user says no/NO
+            check = !input.equals("no");
+            
+        }
+    }
+
+    public static boolean isPalindrome(String str){
+        // Points to the first and the last character of the string
+        int i = 0, j = str.length() - 1;
+        // Check to see if char at i and char at j are the same or not
+        while (i < j){
+            // Return false if they are not the same
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            // Increment i and decrement j
+            i++;
+            j--;
+        }
+        // If they are the same, return true
+        return true;
+    }
+}
